299310436addb3ebca19acbe9457e5874ba9ad38
Kristina Gaevskaya <gaevskaya-2011@mail.ru>
2024-11-27 14:14:24 +0300

    Добавлен вызов метода doSomething()

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,10 +4,8 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("----=");
         System.out.println("Hello World!");
-        System.out.println("1111112");
-        System.out.println("222");
-        System.out.println("333");
-        System.out.println("----");
+        System.out.println("1001");
+        doSomething();
     }
 
     public static void doSomething() {
