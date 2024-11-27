c7c68fb51add3e008f0c040af96ecf7f64a3a23f
Kristina Gaevskaya <gaevskaya-2011@mail.ru>
2024-11-27 13:48:13 +0300

    Улучшили логику main и doSomething()

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -6,11 +6,12 @@ public class MainApplication {
         System.out.println("1111112");
         System.out.println("222");
         System.out.println("333");
+        System.out.println("----");
     }
 
     public static void doSomething() {
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(0);
+        System.out.println(0);
     }
 }
